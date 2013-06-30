61f4e8c44228275960d78bbbce0ae37e774431aa
Ken Lu <ken@seeice.com>
2013-06-30 15:33:29 -0400

    init rails project for the relation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polymorphic::Application.config.secret_key_base = '16742acc7dd88575d99c5506a4492b028c1dc40e84c1e1536fc7ab14eb98fd90834c064c161d74af86e9c0cdfe6833bfcfa05a30544bfa31fcf0dddf97b9a4a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Polymorphic::Application.config.session_store :cookie_store, key: '_polymorphic_session'
