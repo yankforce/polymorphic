558a6d0102cf9b914dee29fb2c56cfab75d5fff4
Ken Lu <ken@seeice.com>
2013-06-30 16:05:56 -0400

    rollback another scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Polymorphic::Application.routes.draw do
-  resources :issues
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
